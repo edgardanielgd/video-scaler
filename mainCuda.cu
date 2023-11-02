88a408fe29be1a141815ea58c22e8dbdeac419c5
edgardanielgd <edgardanielgd123@gmail.com>
2023-11-01 21:16:10 -0500

    Updated run_all sh script

--- mainCuda.cu
+++ mainCuda.cu
@@ -8,8 +8,8 @@
 #include <chrono>
 #include <math.h>
 #include "omp.h"
-
 #include <stdio.h>
+
 #include <cuda_runtime.h>
 
 #define NUM_BLOCKS 40
@@ -110,127 +110,121 @@ cv::Mat *process_video(cv::Mat frames[], int frameCount)
     // Number of iterations
     int nIterations = ceil((double)frameCount / (double)FRAMES_BATCH_SIZE);
 
-    for (int i = 0; i < nIterations; i++)
+    // Calculate number of pixels to process in this batch
+    int output_pixels_to_process = frameCount * output_pixels_per_frame;
+    int input_pixels_to_process = frameCount * input_pixels_per_frame;
+
+    // Calculate number of pixels per thread
+    int pixels_per_thread = output_pixels_to_process / availableThreads;
+
+    std::cout << "Pixels per thread: " << pixels_per_thread << std::endl;
+
+    // Allocate host memory output frames array
+    // Recall that each pixel has 3 channels (RGB)
+    size_t input_data_size = input_pixels_to_process * 3 * sizeof(unsigned char);
+    size_t output_data_size = output_pixels_to_process * 3 * sizeof(unsigned char);
+
+    unsigned char *d_input_data, *d_output_data;
+
+    // Allocate device memory for input and output frames arrays
+    err = cudaMalloc((void **)&d_input_data, input_data_size);
+
+    if (err != cudaSuccess)
     {
-        // Calculate number of frames to process in this batch (last batch may be smaller)
-        int frames_to_process = FRAMES_BATCH_SIZE;
-        if (i == nIterations - 1)
-        {
-            frames_to_process = frameCount - i * FRAMES_BATCH_SIZE;
-        }
+        fprintf(stderr, "Failed to allocate device input vector (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
 
-        // Calculate number of pixels to process in this batch
-        int output_pixels_to_process = frames_to_process * output_pixels_per_frame;
-        int input_pixels_to_process = frames_to_process * input_pixels_per_frame;
+    // Assign input data to host memory
+    for (int j = 0; j < frameCount; j++)
+    {
+        int frame_index = j;
 
-        // Calculate number of pixels per thread
-        int pixels_per_thread = output_pixels_to_process / availableThreads;
-
-        // Allocate host memory output frames array
-        // Recall that each pixel has 3 channels (RGB)
-        size_t input_data_size = input_pixels_to_process * 3 * sizeof(unsigned char);
-        size_t output_data_size = output_pixels_to_process * 3 * sizeof(unsigned char);
-
-        unsigned char *d_input_data, *d_output_data;
-
-        // Allocate device memory for input and output frames arrays
-        err = cudaMalloc((void **)&d_input_data, input_data_size);
+        // Copy input frame to host memory
+        err = cudaMemcpy(
+            d_input_data + j * input_pixels_per_frame * 3,
+            frames[frame_index].data,
+            input_pixels_per_frame * 3 * sizeof(unsigned char),
+            cudaMemcpyHostToDevice);
 
         if (err != cudaSuccess)
         {
-            fprintf(stderr, "Failed to allocate device input vector (error code %s)!\n", cudaGetErrorString(err));
+            std::cout << "Frame index: " << frame_index << std::endl;
+            fprintf(
+                stderr,
+                "Failed to assign input vector frames data at frame index %d (error code %s)!\n",
+                frame_index, cudaGetErrorString(err));
+
             exit(EXIT_FAILURE);
         }
+    }
 
-        // Assign input data to host memory
-        for (int j = 0; j < frames_to_process; j++)
-        {
-            int frame_index = i * FRAMES_BATCH_SIZE + j;
+    // Allocate device memory for output frames array
+    err = cudaMalloc((void **)&d_output_data, output_data_size);
 
-            // Copy input frame to host memory
-            err = cudaMemcpy(
-                d_input_data + j * input_pixels_per_frame * 3,
-                frames[frame_index].data,
-                input_pixels_per_frame * 3 * sizeof(unsigned char),
-                cudaMemcpyHostToDevice);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to allocate device output vector (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
 
-            if (err != cudaSuccess)
-            {
-                fprintf(
-                    stderr,
-                    "Failed to assign input vector frames data at frame index %d (error code %s)!\n",
-                    frame_index, cudaGetErrorString(err));
-                exit(EXIT_FAILURE);
-            }
-        }
+    // Data is assigned and memory allocated, now process pixels in parallel
+    process<<<num_blocks, num_threads_per_block>>>(
+        d_input_data, d_output_data,
+        pixels_per_thread, frameCount,
+        input_width, input_height,
+        output_width, output_height);
 
-        // Allocate device memory for output frames array
-        err = cudaMalloc((void **)&d_output_data, output_data_size);
+    err = cudaGetLastError();
+
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to launch process kernel (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    // Copy frame data from host memory to output frames array
+
+    for (int j = 0; j < frameCount; j++)
+    {
+        int frame_index = j;
+
+        cv::Mat output_frame(output_height, output_width, CV_8UC3);
+
+        // Copy input frame to host memory
+        err = cudaMemcpy(
+            output_frame.data,
+            d_output_data + j * output_pixels_per_frame * 3,
+            output_pixels_per_frame * 3 * sizeof(unsigned char),
+            cudaMemcpyDeviceToHost);
 
         if (err != cudaSuccess)
         {
-            fprintf(stderr, "Failed to allocate device output vector (error code %s)!\n", cudaGetErrorString(err));
+            fprintf(
+                stderr,
+                "Failed to assign output vector frames data at frame index %d (error code %s)!\n",
+                frame_index, cudaGetErrorString(err));
             exit(EXIT_FAILURE);
         }
 
-        // Data is assigned and memory allocated, now process pixels in parallel
-        process<<<num_blocks, num_threads_per_block>>>(
-            d_input_data, d_output_data,
-            pixels_per_thread, frames_to_process,
-            input_width, input_height,
-            output_width, output_height);
+        output_frames[frame_index] = output_frame;
+    }
 
-        err = cudaGetLastError();
+    // Free device global memory
+    err = cudaFree(d_input_data);
 
-        if (err != cudaSuccess)
-        {
-            fprintf(stderr, "Failed to launch process kernel (error code %s)!\n", cudaGetErrorString(err));
-            exit(EXIT_FAILURE);
-        }
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to free device vector A (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
 
-        // Copy frame data from host memory to output frames array
+    err = cudaFree(d_output_data);
 
-        for (int j = 0; j < frames_to_process; j++)
-        {
-            int frame_index = i * FRAMES_BATCH_SIZE + j;
-
-            cv::Mat output_frame(output_height, output_width, CV_8UC3);
-
-            // Copy input frame to host memory
-            err = cudaMemcpy(
-                output_frame.data,
-                d_output_data + j * output_pixels_per_frame * 3,
-                output_pixels_per_frame * 3 * sizeof(unsigned char),
-                cudaMemcpyDeviceToHost);
-
-            if (err != cudaSuccess)
-            {
-                fprintf(
-                    stderr,
-                    "Failed to assign output vector frames data at frame index %d (error code %s)!\n",
-                    frame_index, cudaGetErrorString(err));
-                exit(EXIT_FAILURE);
-            }
-
-            output_frames[frame_index] = output_frame;
-        }
-
-        // Free device global memory
-        err = cudaFree(d_input_data);
-
-        if (err != cudaSuccess)
-        {
-            fprintf(stderr, "Failed to free device vector A (error code %s)!\n", cudaGetErrorString(err));
-            exit(EXIT_FAILURE);
-        }
-
-        err = cudaFree(d_output_data);
-
-        if (err != cudaSuccess)
-        {
-            fprintf(stderr, "Failed to free device vector B (error code %s)!\n", cudaGetErrorString(err));
-            exit(EXIT_FAILURE);
-        }
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to free device vector B (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
     }
 
     return output_frames;
@@ -240,7 +234,6 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    auto start = chrono::high_resolution_clock::now();
     // Get params size
     if (argc >= 5)
     {
@@ -279,12 +272,14 @@ int main(int argc, char *argv[])
     input_height = capture.get(cv::CAP_PROP_FRAME_HEIGHT);
     int frame_count = capture.get(cv::CAP_PROP_FRAME_COUNT);
     int fps = capture.get(cv::CAP_PROP_FPS);
+    int video_time = frame_count / fps;
 
     cout << "Video metadata: (" << num_threads_per_block << ")" << endl;
     cout << "  - Frame width: " << input_width << endl;
     cout << "  - Frame height: " << input_height << endl;
     cout << "  - Frame count: " << frame_count << endl;
     cout << "  - FPS: " << fps << endl;
+    cout << "Duration: " << video_time << " seconds" << endl;
 
     cout << "Output video metadata:" << endl;
     cout << "  - Frame width: " << output_width << endl;
@@ -295,62 +290,84 @@ int main(int argc, char *argv[])
 
     assert(input_width >= output_width && input_height >= output_height);
 
+    // Measure total duration for writes, reads and processing
+    uint64_t total_write_duration = 0;
+    uint64_t total_read_duration = 0;
+    uint64_t total_process_duration = 0;
+
     // Copying video frame by frame into video writter
-    cv::VideoWriter writter(output_filename, cv::VideoWriter::fourcc('a', 'v', 'c', '1'), fps, cv::Size(output_width, output_height));
+    cv::VideoWriter writter(
+        output_filename,
+        cv::VideoWriter::fourcc('M', 'J', 'P', 'G'),
+        fps, cv::Size(output_width, output_height)
+    );
 
-    // We'll write output video frame by frame in parallel
-    cv::Mat frames[frame_count];
+    int nIterations = ceil((double)frame_count / (double)FRAMES_BATCH_SIZE);
 
-    auto read_start = chrono::high_resolution_clock::now();
-    cout << "Reading video frames..." << endl;
-    for (int frame_number = 0; frame_number < frame_count; frame_number++)
+    for (int i = 0; i < nIterations; i++)
     {
-        capture >> frames[frame_number];
+        cout << "Processing batch " << i << " of " << nIterations - 1 << endl;
+        // Calculate number of frames to process in this batch (last batch may be smaller)
+        int frames_to_process = FRAMES_BATCH_SIZE;
+        if (i == nIterations - 1)
+        {
+            frames_to_process = frame_count - i * FRAMES_BATCH_SIZE;
+        }
+
+        cv::Mat *frames = new cv::Mat[frames_to_process];
+
+        auto start_read = chrono::high_resolution_clock::now();
+
+        for (int dummy_frame_number = 0; dummy_frame_number < frames_to_process; dummy_frame_number++)
+        {
+            cv::Mat frame;
+            capture >> frame;
+
+            // Fill empty frames with previous frame
+
+            if (frame.empty())
+            {
+                frame = frames[dummy_frame_number - 1].clone();
+            }
+
+            frames[dummy_frame_number] = frame;
+        }
+
+        auto end_read = chrono::high_resolution_clock::now();
+        auto duration_read = chrono::duration_cast<chrono::nanoseconds>(end_read - start_read);
+        total_read_duration += duration_read.count();
+
+        auto start_process = chrono::high_resolution_clock::now();
+        cv::Mat *outputs = process_video(frames, frames_to_process);
+
+        auto end_process = chrono::high_resolution_clock::now();
+        auto duration_process = chrono::duration_cast<chrono::nanoseconds>(end_process - start_process);
+        total_process_duration += duration_process.count();
+
+        auto start_write = chrono::high_resolution_clock::now();
+
+        for (int dummy_frame_number = 0; dummy_frame_number < frames_to_process; dummy_frame_number++)
+        {
+            writter << outputs[dummy_frame_number];
+        }
+
+        auto end_write = chrono::high_resolution_clock::now();
+        auto duration_write = chrono::duration_cast<chrono::nanoseconds>(end_write - start_write);
+        total_write_duration += duration_write.count();
+
+        // Freeing memory
+        delete[] frames;
+        delete[] outputs;
     }
 
-    auto read_end = chrono::high_resolution_clock::now();
-    auto read_duration = chrono::duration_cast<chrono::nanoseconds>(read_end - read_start);
+    cout << "Blocks: " << num_blocks << " Threads: " << num_threads_per_block << " Processing Time: " << total_process_duration << endl;
 
-    cout << "Read time: " << read_duration.count() << endl;
+    uint64_t total_duration = total_read_duration + total_process_duration + total_write_duration;
 
-    capture.release();
+    cout << "Total duration: " << total_duration << endl;
+    cout << "Total read duration: " << total_read_duration << endl;
+    cout << "Total write duration: " << total_write_duration << endl;
 
-    cout << "Processing video..." << endl;
-
-    auto process_start = chrono::high_resolution_clock::now();
-    cv::Mat *outputs = process_video(frames, frame_count);
-
-    auto process_end = chrono::high_resolution_clock::now();
-    auto process_duration = chrono::duration_cast<chrono::nanoseconds>(process_end - process_start);
-
-    cout << "Processing time: " << process_duration.count() << endl;
-
-    // Joining all frames into a single video
-
-    auto write_start = chrono::high_resolution_clock::now();
-    cout << "Reading video frames..." << endl;
-
-    cout << "Writing video..." << endl;
-    for (int frame_number = 0; frame_number < frame_count; frame_number++)
-    {
-        writter << outputs[frame_number];
-    }
-
-    auto write_end = chrono::high_resolution_clock::now();
-    auto write_duration = chrono::duration_cast<chrono::nanoseconds>(write_end - write_start);
-
-    cout << "Write time: " << write_duration.count() << endl;
-
-    writter.release();
-
-    printf("Done\n");
-
-    // Freeing memory
-    delete[] outputs;
-
-    auto end = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
-
-    cout << "Blocks: " << num_blocks << "Threads: " << num_threads_per_block << " Execution Time: " << duration.count() << endl;
+    cout << "Processing vs duration: " << 1e9 * (double)video_time / (double)total_duration << endl;
     return 0;
 }
--- run_all.sh
+++ run_all.sh
@@ -15,14 +15,14 @@
 nvcc mainCuda.cu -o main_cuda $(pkg-config --cflags --libs opencv4)
 
 # Run code for multiple cases of different BlockSizes and GridSizes
-./main_cuda $1 $2 1 1 >> $4
+./main_cuda $1 $2 1 1 >> $3
 
-for i in 40 50 60 70
+for i in 10 20 40 80 160
 do
-    for j in 128 256 512 1024
+    for j in 4 16 32 64 128 256 512
     do
         echo "Running for BlockSize: $j, GridSize: $i"
-        ./main_cuda $1 $2 $j $i >> $4
-        echo "\n\n" >> $4
+        ./main_cuda $1 $2 $j $i >> $3
+        echo "\n\n" >> $3
     done
 done
